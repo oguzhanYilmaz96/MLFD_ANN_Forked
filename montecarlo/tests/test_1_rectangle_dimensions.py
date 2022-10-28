a6e4dd719fb6e01e79bec4fccc652ddfd6bdcf78
Fran <fratorhe@vki.ac.be>
2022-10-28 17:38:38 +0200

    fixed test

--- montecarlo/tests/test_1_rectangle_dimensions.py
+++ montecarlo/tests/test_1_rectangle_dimensions.py
@@ -5,4 +5,4 @@ def test_check_rectangle_dimensions(area_rectangle):
     assert area_rectangle.x0 == -1
     assert area_rectangle.x1 == 2
     assert area_rectangle.y0 == 1
-    assert area_rectangle.10 == 5
+    assert area_rectangle.y1 == 5
