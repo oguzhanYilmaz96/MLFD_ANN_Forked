4e6cb2a476db6481c726926bd736307650b6e6df
Fran <fratorhe@vki.ac.be>
2022-10-28 17:36:56 +0200

    fixed test

--- montecarlo/tests/test_1_rectangle_dimensions.py
+++ montecarlo/tests/test_1_rectangle_dimensions.py
@@ -3,6 +3,6 @@ from .aux_methods import area_rectangle
 
 def test_check_rectangle_dimensions(area_rectangle):
     assert area_rectangle.x0 == -1
-    raise NotImplementedError
-    # TODO: YOUR CODE HERE:
-    # implement for the remaining corners of the box. See aux_methods.py for help.
+    assert area_rectangle.x1 == 2
+    assert area_rectangle.y0 == 1
+    assert area_rectangle.10 == 5
